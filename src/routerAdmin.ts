79ad599f43164b07fc141c7174c7c2e83f94668b
jarvis-control <shakh_mac3@icloud.com>
2026-03-16 02:39:25 +0900

    feat: Create checkAuthSession API

--- src/routerAdmin.ts
+++ src/routerAdmin.ts
@@ -13,6 +13,8 @@ routerAdmin
   .get("/signup", adminController.getSignup)
   .post("/signup", adminController.processSignup);
 
+routerAdmin.get("/check-me", adminController.checkAuthSession);
+
 /** Product */
 /** User */
 
