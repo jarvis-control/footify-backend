63cb2a03006fc70eaa53459a36acb15cea3b6525
jarvis-control <shakh_mac3@icloud.com>
2026-03-16 01:40:29 +0900

    feat: Add frontend CSS, JS, and image files

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -12,7 +12,7 @@ const adminController: T = {};
 adminController.goHome = (req: Request, res: Response) => {
   try {
     console.log("goHome");
-    res.send("Home Page");
+    res.render("home"); // home.ejs
   } catch (err) {
     console.log("Error, goHome", err);
   }
@@ -21,7 +21,7 @@ adminController.goHome = (req: Request, res: Response) => {
 adminController.getSignup = (req: Request, res: Response) => {
   try {
     console.log("getSignup");
-    res.send("Signup Page");
+    res.render("signup"); // signup.ejs
   } catch (err) {
     console.log("Error, getSignup", err);
   }
@@ -30,7 +30,7 @@ adminController.getSignup = (req: Request, res: Response) => {
 adminController.getLogin = (req: Request, res: Response) => {
   try {
     console.log("getLogin");
-    res.send("Login Page");
+    res.render("login"); // login.ejs
   } catch (err) {
     console.log("Error, getLogin", err);
   }
