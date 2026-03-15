e1793ede11e4189d7eeb13eefa4cea0d7c8a75bc
jarvis-control <shakh_mac3@icloud.com>
2026-03-15 16:30:05 +0900

    feat: Develop Admin signup process

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
+import { MemberInput } from "../libs/types/member";
+import { MemberType } from "../libs/enums/member.enum";
+
+const memberService = new MemberService();
 
 const adminController: T = {};
 
@@ -40,10 +44,17 @@ adminController.getSignup = (req: Request, res: Response) => {
   }
 };
 
-adminController.processSignup = (req: Request, res: Response) => {
+adminController.processSignup = async (req: Request, res: Response) => {
   try {
     console.log("processSignup");
-    res.send("DONE");
+    console.log("body:", req.body);
+
+    const newMember: MemberInput = req.body;
+    newMember.memberType = MemberType.ADMIN;
+
+    const result = await memberService.processSignup(newMember);
+
+    res.send(result);
   } catch (err) {
     console.log("Error, processSignup", err);
   }
