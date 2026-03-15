56dd9032a158a6ce61f6c5a7969c2ffed0677c2e
jarvis-control <shakh_mac3@icloud.com>
2026-03-16 02:05:31 +0900

    feat: Integrate session middleware

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,15 @@ import routerAdmin from "./routerAdmin";
 import morgan from "morgan";
 import { MORGAN_FORMAT } from "./libs/config";
 
+import session from "express-session";
+import ConnectMongodb from "connect-mongodb-session";
+
+const MongoDBStore = ConnectMongodb(session);
+const store = new MongoDBStore({
+  uri: String(process.env.MONGO_URL),
+  collection: "sessions",
+});
+
 /** 1-ENTRANCE */
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
@@ -13,6 +22,17 @@ app.use(express.json());
 app.use(morgan(MORGAN_FORMAT));
 
 /** 2-SESSIONS */
+app.use(
+  session({
+    secret: String(process.env.SESSION_SECRET),
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 3, // 3h
+    },
+    store: store,
+    resave: true, // 10:00 => 13:00 | 12:00 => 15:00
+    saveUninitialized: true,
+  }),
+);
 
 /** 3-VIEWS */
 app.set("views", path.join(__dirname, "views"));
