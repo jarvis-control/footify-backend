2e7d698935885b2f1919e53abeccee244f74c743
jarvis-control <shakh_mac3@icloud.com>
2026-03-26 20:14:02 +0900

    feat: Create product controller and routing APIs

--- src/routerAdmin.ts
+++ src/routerAdmin.ts
@@ -1,6 +1,7 @@
 import express from "express";
 const routerAdmin = express.Router();
 import adminController from "./controllers/admin.controller";
+import productController from "./controllers/product.controller";
 
 /** Admin */
 routerAdmin.get("/", adminController.goHome);
@@ -18,6 +19,10 @@ routerAdmin.get("/logout", adminController.logout);
 routerAdmin.get("/check-me", adminController.checkAuthSession);
 
 /** Product */
+routerAdmin.get("/product/all", productController.getAllProducts);
+routerAdmin.post("/product/create", productController.createNewProduct);
+routerAdmin.post("/product/:id", productController.updateChosenProduct);
+
 /** User */
 
 export default routerAdmin;
