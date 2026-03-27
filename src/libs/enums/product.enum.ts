57aec5eac4ad30139274f3767e03e44d733baf13
jarvis-control <shakh_mac3@icloud.com>
2026-03-28 00:53:32 +0900

    feat: Configure product schema model

---
+++ src/libs/enums/product.enum.ts
@@ -0,0 +1,47 @@
+export enum ProductStatus {
+  PAUSE = "PAUSE",
+  PROCESS = "PROCESS",
+  DELETE = "DELETE",
+}
+
+export enum ProductCategory {
+  KIT = "KIT",
+  BOOTS = "BOOTS",
+  BALL = "BALL",
+  GLOVES = "GLOVES",
+}
+
+export enum ProductSize {
+  XS = "XS",
+  S = "S",
+  M = "M",
+  L = "L",
+  XL = "XL",
+  XXL = "XXL",
+  EU_38 = "EU_38",
+  EU_39 = "EU_39",
+  EU_40 = "EU_40",
+  EU_41 = "EU_41",
+  EU_42 = "EU_42",
+  EU_43 = "EU_43",
+  EU_44 = "EU_44",
+  EU_45 = "EU_45",
+  SIZE_3 = "SIZE_3",
+  SIZE_4 = "SIZE_4",
+  SIZE_5 = "SIZE_5",
+}
+
+export enum KitType {
+  JERSEY = "JERSEY",
+  SHORTS = "SHORTS",
+  SOCKS = "SOCKS",
+}
+
+export enum ProductBrand {
+  NIKE = "NIKE",
+  ADIDAS = "ADIDAS",
+  PUMA = "PUMA",
+  NEW_BALANCE = "NEW_BALANCE",
+  UNDER_ARMOUR = "UNDER_ARMOUR",
+  UMBRO = "UMBRO",
+}
---
+++ src/schema/Product.model.ts
@@ -0,0 +1,93 @@
+import mongoose, { Schema } from "mongoose";
+import {
+  KitType,
+  ProductBrand,
+  ProductCategory,
+  ProductSize,
+  ProductStatus,
+} from "../libs/enums/product.enum";
+
+// schema
+const productSchema = new Schema(
+  {
+    productStatus: {
+      type: String,
+      enum: ProductStatus,
+      default: ProductStatus.PAUSE,
+    },
+
+    productCategory: {
+      type: String,
+      enum: ProductCategory,
+      required: true,
+    },
+
+    kitType: {
+      type: String,
+      enum: KitType,
+    },
+
+    productBrand: {
+      type: String,
+      enum: ProductBrand,
+      required: true,
+    },
+
+    productName: {
+      type: String,
+      required: true,
+    },
+
+    productPrice: {
+      type: Number,
+      required: true,
+    },
+
+    productLeftCount: {
+      type: Number,
+      required: true,
+    },
+
+    productSize: {
+      type: String,
+      enum: ProductSize,
+      required: true,
+    },
+
+    productColor: {
+      type: String,
+    },
+
+    productDesc: {
+      type: String,
+      required: true,
+    },
+
+    productImages: {
+      type: [String],
+      required: true,
+    },
+
+    productViews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+
+    productLikes: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  { timestamps: true }, // createdAt, updatedAt
+);
+
+// Duplicate prevention
+productSchema.index(
+  { productName: 1, productSize: 1, productBrand: 1 },
+  { unique: true },
+);
+
+// model
+export default mongoose.model("Product", productSchema); // productModel
