30a8f43eb3c5ff02e9bf141d531a9fc810912603
jarvis-control <shakh_mac3@icloud.com>
2026-04-16 00:27:15 +0900

    feat: Add logout() and verifyAuth() APIs

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
-import { LoginInput, MemberInput } from "../libs/types/member";
+import { ExtendedRequest, LoginInput, MemberInput } from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import AuthService from "../models/Auth.service";
 import { AUTH_TIMER } from "../libs/config";
@@ -54,6 +54,18 @@ memberController.login = async (req: Request, res: Response) => {
   }
 };
 
+memberController.logout = (req: ExtendedRequest, res: Response) => {
+  try {
+    console.log("logout");
+    res.cookie("accessToken", null, { maxAge: 0, httpOnly: true });
+    res.status(HttpCode.OK).json({ logout: true });
+  } catch (err) {
+    console.log("Error, login:", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+  }
+};
+
 memberController.verifyAuth = async (req: Request, res: Response) => {
   try {
     let member = null;
@@ -69,6 +81,5 @@ memberController.verifyAuth = async (req: Request, res: Response) => {
     else res.status(Errors.standard.code).json(Errors.standard);
   }
 };
-
 
 export default memberController;
