7253fe275980d95454d99d048216c42b479afa7d
jarvis-control <shakh_mac3@icloud.com>
2026-04-16 00:41:59 +0900

    feat: Add getProducts() API

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -91,7 +91,8 @@ memberController.updateMember = async (req: ExtendedRequest, res: Response) => {
   try {
     console.log("updateMember");
     const input: MemberUpdateInput = req.body;
-    if (req.file) input.memberImage = req.file.path;
+    if (req.file) input.memberImage = req.file.path.replace(/\\/, "/");
+
     const result = await memberService.updateMember(req.member, input);
 
     res.status(HttpCode.OK).json(result);
--- src/libs/types/product.ts
+++ src/libs/types/product.ts
@@ -26,6 +26,14 @@ export interface Product {
   updatedAt: Date;
 }
 
+export interface ProductInquiry {
+  order: string;
+  page: number;
+  limit: number;
+  productCategory?: ProductCategory;
+  search?: string;
+}
+
 export interface ProductInput {
   productStatus?: ProductStatus;
   productCategory: ProductCategory;
