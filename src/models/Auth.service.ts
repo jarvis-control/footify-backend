814c86c1f9095980d65e3f55c3cd7016af521791
jarvis-control <shakh_mac3@icloud.com>
2026-04-16 00:20:45 +0900

    feat: Add verifyAuth() - authenticated user verification logic

--- src/models/Auth.service.ts
+++ src/models/Auth.service.ts
@@ -4,11 +4,14 @@ import { Member } from "../libs/types/member";
 import jwt from "jsonwebtoken";
 
 class AuthService {
-  constructor() {}
+  private readonly secretToken;
+  constructor() {
+    this.secretToken = process.env.SECRET_TOKEN as string;
+  }
 
   public async createToken(payload: Member) {
     return new Promise((resolve, reject) => {
-      const durartion = `${AUTH_TIMER}`;
+      const durartion = `${AUTH_TIMER}h`;
       jwt.sign(
         payload,
         process.env.SECRET_TOKEN as string,
@@ -24,6 +27,15 @@ class AuthService {
         },
       );
     });
+  }
+
+  public async checkAuth(token: string): Promise<Member> {
+    const result: Member = (await jwt.verify(
+      token,
+      this.secretToken,
+    )) as Member;
+    console.log(`--- [AUTH] memberNick: ${result.memberNick} ---`);
+    return result;
   }
 }
 
