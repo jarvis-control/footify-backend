866adb068a4da4c6a64cbbe31d4972a0473b3482
jarvis-control <shakh_mac3@icloud.com>
2026-04-16 00:16:13 +0900

    feat: Implement cookie-based token authentication

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -2,8 +2,9 @@ import { Request, Response } from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
 import { LoginInput, MemberInput } from "../libs/types/member";
-import Errors from "../libs/Errors";
+import Errors, { HttpCode } from "../libs/Errors";
 import AuthService from "../models/Auth.service";
+import { AUTH_TIMER } from "../libs/config";
 
 // REACT
 
@@ -19,7 +20,12 @@ memberController.signup = async (req: Request, res: Response) => {
     const result = await memberService.signup(input),
       token = await authService.createToken(result);
 
-    res.json({ member: result });
+    res.cookie("accessToken", token, {
+      maxAge: AUTH_TIMER * 3600 * 1000,
+      httpOnly: false,
+    });
+
+    res.status(HttpCode.CREATED).json({ member: result, accessToken: token });
   } catch (err) {
     console.log("Error, signup", err);
     if (err instanceof Errors) res.status(err.code).json(err);
@@ -35,7 +41,12 @@ memberController.login = async (req: Request, res: Response) => {
     const result = await memberService.login(input),
       token = await authService.createToken(result);
 
-    res.json({ member: result });
+    res.cookie("accessToken", token, {
+      maxAge: AUTH_TIMER * 3600 * 1000,
+      httpOnly: false,
+    });
+
+    res.status(HttpCode.OK).json({ member: result, accessToken: token });
   } catch (err) {
     console.log("Error, login", err);
     if (err instanceof Errors) res.status(err.code).json(err);
